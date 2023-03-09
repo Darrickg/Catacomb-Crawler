a83164295d5d01c79039aeda2e74ec80bb0162a6
hanxic <hanxic@sfu.ca>
2023-03-08 20:11:30 -0800

    Create Entity class

---
+++ src/main/java/Entity/Entity.java
@@ -0,0 +1,15 @@
+package Entity;
+
+public class Entity {
+    public int x,y;
+    public int speed;
+
+    public int getHorizontalPosition(){
+        return x;
+    }
+    public int getVerticalPosition(){
+        return y;
+    }
+
+
+}
