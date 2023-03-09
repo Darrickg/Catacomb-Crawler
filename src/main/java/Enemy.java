c6129c582c39b880c03d35c1a6d93d7f90140c79
msa294 <msa294@sfu.ca>
2023-03-09 14:18:03 -0800

    made changes to enemy class

--- src/main/java/Enemy.java
+++ src/main/java/Enemy.java
@@ -1,17 +1,51 @@
-public abstract class Enemy extends Entity{
-        protected int damage; // amount of damage the enemy does to the player
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
-        public Enemy(int damage) {
-            this.damage = damage;
-        }
+public abstract class Enemy {
+    private int x;
+    private int y;
+    protected int damage; // amount of damage the enemy does to the player
 
-        public int getDamage() {
-            return damage;
-        }
+    public Enemy(int x, int y,int damage) {
+        this.damage = damage;
+        this.x =x;
+        this.y=y;
 
-        // abstract method to handle enemy behavior
-        public abstract void handleCollision(Player player);
     }
+
+
+
+    public int getDamage() {
+        return damage;
+    }
+
+    // override the abstract method to handle moving enemy behavior
+    public abstract void handleCollision(Player player);
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void draw(Graphics2D g2d) {
+    }
+}
+
+
 
 
 
