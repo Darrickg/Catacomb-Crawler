f32c68bd9a0529dfc8630355af3747662865663d
msa294 <msa294@sfu.ca>
2023-03-15 14:13:03 -0700

    added buttons instead of keyboard actions for mainmenu state

--- src/main/java/GameStates/MainMenuState.java
+++ src/main/java/GameStates/MainMenuState.java
@@ -2,15 +2,36 @@ package GameStates;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class MainMenuState extends JPanel implements GameState, KeyListener {
+public class MainMenuState extends JPanel implements GameState, ActionListener {
     private static final int SCREEN_WIDTH = 800;
     private static final int SCREEN_HEIGHT = 600;
     private GameStateManager stateManager = new GameStateManager();
     JFrame frame = new JFrame("Main Menu");
+    private JButton startButton;
+    private JButton exitButton;
     public void init() {
+        // Create the start button
+        startButton = new JButton("Start Game");
+        startButton.setBounds(100, 100, 100, 50); // x, y, width, height
+        startButton.setFont(new Font("Arial", Font.BOLD, 20)); // font name, style, size
+        startButton.setBackground(Color.GREEN);
+        startButton.setForeground(Color.WHITE);
+        startButton.addActionListener(this);
+        add(startButton);
+
+        // Create the exit button
+        exitButton = new JButton("Exit Game");
+        exitButton.addActionListener(this);
+        exitButton.setBounds(250, 100, 100, 50); // x, y, width, height
+        exitButton.setFont(new Font("Arial", Font.BOLD, 20)); // font name, style, size
+        exitButton.setBackground(Color.RED);
+        exitButton.setForeground(Color.WHITE);
+        add(exitButton);
 
         // Initialize the main menu state.
         setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
@@ -18,7 +39,6 @@ public class MainMenuState extends JPanel implements GameState, KeyListener {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(1000, 1000);
         frame.setResizable(false);
-        frame.addKeyListener(this);
         frame.add(this);
         frame.setVisible(true);
     }
@@ -32,31 +52,24 @@ public class MainMenuState extends JPanel implements GameState, KeyListener {
     }
 
 
-    @Override
-    public void keyTyped(KeyEvent e) {
-
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-        int k = e.getKeyCode();
-        if (k == KeyEvent.VK_ENTER) {
-            if (stateManager.getCurrentState() != null) {
-                System.out.println("Closing MenuState");
-                frame.dispose();
-            }
-            getStateManager().setState(new RunningState());
-        }
-    }
-
 
     private GameStateManager getStateManager() {
         return stateManager;
     }
 
 
-    @Override
-    public void keyReleased(KeyEvent e) {
 
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == startButton) {
+            if (stateManager.getCurrentState() != null) {
+                System.out.println("Closing MenuState");
+                frame.dispose();
+            }
+            getStateManager().setState(new RunningState());
+        } else if (e.getSource() == exitButton) {
+            System.exit(0);
+        }
     }
 }
