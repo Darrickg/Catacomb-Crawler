22a975134bcbb60304fe05820cd12638e2fe16a2
hanxic <hanxic@sfu.ca>
2023-03-17 02:02:32 -0700

    Player JavaDoc update

--- src/main/java/Entity/Player.java
+++ src/main/java/Entity/Player.java
@@ -20,7 +20,9 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import java.awt.Rectangle;
 
-
+/**
+ * The player class extends from entity represent a generic player object
+ */
 public class Player extends Entity{
     // Attributes
     private int score = 100;
@@ -41,8 +43,14 @@ public class Player extends Entity{
     private BufferedImage[] sprites;
     private int currentFrame;
 
-
-    // constructor
+    /**
+     * Player constructor with all fields as parameters
+     * @param x dimension x
+     * @param y dimension y
+     * @param playerWidth width of player model
+     * @param PlayerHeight height of player model
+     * @param lives lives of player model
+     */
     public Player(int x, int y , int playerWidth, int PlayerHeight, int lives ) {
         this.x = x;
         this.y = y;
@@ -69,35 +77,73 @@ public class Player extends Entity{
         }
     }
 
-    // Getters
+    /**
+     * Player score getter
+     * @return player score
+     */
     public int  getScore(){ return this.score;}
+
+    /**
+     * Player lives getter
+     * @return player lives
+     */
     public int getLives(){ return this.lives;}
 
-    //public ArrayList getInventory() {
-    //return inventory;
-    //}
-
-    // Setters
+    /**
+     * player score setter
+     * @param value set value to score
+     */
     public void setScore(int value){ this.score = value;}
+
+    /**
+     * player lives setter
+     * @param value set value to lives
+     */
     public void setLives(int value){ this.lives = value;}
+
+    /**
+     * abandon method
+     * @param inventory nill
+     */
     public void setInventory(ArrayList inventory) {
         inventory = inventory;
     }
-    // Adders
+
+    /**
+     * player score adder
+     * @param dif add dif to score
+     */
     public void addScore(int dif){ this.score += dif;}
+
+    /**
+     * player score decreaser
+     * @param diff decrease score to player
+     */
     public void decreaseScore(int diff){ this.score-=diff;}
+
+    /**
+     * player lives adder
+     * @param dif add dif to player lives
+     */
     public void addLives(int dif){ this.lives += dif;}
 
     // TODO: Hitbox config
 
     // TODO: Movement
     // move the player
+
+    /**
+     * abandon method
+     */
     public void move() {
         x += vx;
         y += vy;
         hitbox.setLocation(getX(),getY());
     }
 
+    /**
+     * store and update player positiom
+     */
     public void update() {
         // Store previous position
         prevX = x;
@@ -108,117 +154,204 @@ public class Player extends Entity{
         x += vx;
         y += vy;
     }
+
+    /**
+     * player position setter
+     * @param x set x to x
+     * @param y set y to y
+     */
     public void setPosition(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * player revert position
+     */
     public void revertPosition() {
         // Revert the player's position to the previous position
         x = prevX;
         y = prevY;
     }
 
+    /**
+     * player hitBox getter
+     * @return player model hitBox
+     */
     public Rectangle getHitbox(){
         return hitbox;
     }
 
-    // move player up
+    /**
+     * move player up
+     */
     public void moveUp() {
         vy = -2;
     }
 
-    // move player down
+    /**
+     * move player down
+     */
     public void moveDown() {
         vy = 2;
     }
 
-    // move player left
+    /**
+     * move player left
+     */
     public void moveLeft() {
         vx = -2;
         currentFrame = 1;
     }
 
-    // move player right
+    /**
+     * move player right
+     */
     public void moveRight() {
         vx = 2;
         currentFrame = 0;
     }
 
-    // stop player movement in x axis
+    /**
+     * stop player at x
+     */
     public void stopX() {
         vx = 0;
     }
 
-    // stop player movement in y axis
+    /**
+     * stop player at y
+     */
     public void stopY() {
         vy = 0;
     }
 
-    // getters and setters
+    /**
+     * player x direction getter
+     * @return x
+     */
     public int getX() {
         return x;
     }
 
+    /**
+     * player x direction setter
+     * @param x set x to x
+     */
     public void setX(int x) {
 
         this.x = x;
     }
 
+    /**
+     * player y direction getter
+     * @return y
+     */
     public int getY() {
         return y;
     }
 
+    /**
+     * player y direction setter
+     * @param y set y to y
+     */
     public void setY(int y) {
 
         this.y = y;
     }
 
+    /**
+     * abandon method
+     * @return vx
+     */
     public int getVx() {
         return vx;
     }
 
+    /**
+     * abandon method
+     * @param vx to vx
+     */
     public void setVx(int vx) {
         this.vx = vx;
     }
 
+    /**
+     * abandon method
+     * @return vy
+     */
     public int getVy() {
         return vy;
     }
 
+    /**
+     * player previous vx getter
+     * @return prev vx
+     */
     public int getPrevX() {
         return prevX;
     }
 
+    /**
+     * player previous vx setters
+     * @param prevX set vx = vx
+     */
     public void setPrevX(int prevX) {
         this.prevX = prevX;
     }
 
+    /**
+     * player previous vy getter
+     * @return prev vy
+     */
     public int getPrevY() {
         return prevY;
     }
 
+    /**
+     * player prev vy setter
+     * @param prevY vy = vy
+     */
     public void setPrevY(int prevY) {
         this.prevY = prevY;
     }
 
+    /**
+     * abandon method
+     * @param vy vy = vy
+     */
     public void setVy(int vy) {
         this.vy = vy;
     }
 
+    /**
+     * player current frame setter
+     * @param currentFrame frane = currentFrame
+     */
     public void setCurrentFrame(int currentFrame) {
         this.currentFrame = currentFrame;
     }
 
-    // Draw player
+    /**
+     * Draw player
+     * @param g2d draw
+     */
     public void draw(Graphics2D g2d){
         g2d.drawImage(sprites[currentFrame], x, y, null);
     }
 
+    /**
+     * whether play can damage
+     * @return can or can't damage
+     */
     public boolean canDamage(){
         return System.currentTimeMillis() - this.lastDamageTime > this.durationBeforeDamage;
     }
 
+    /**
+     * Player take damage
+     * @param damage take damage
+     */
     public void takeDamage(int damage) {
         if(!canDamage())
             return;
@@ -230,18 +363,34 @@ public class Player extends Entity{
         }
     }
 
+    /**
+     * player width getter
+     * @return player width
+     */
     public int getWidth() {
         return width;
     }
 
+    /**
+     * player width setter
+     * @param width width to width
+     */
     public void setWidth(int width) {
         this.width = width;
     }
 
+    /**
+     * player height getter
+     * @return player height
+     */
     public int getHeight() {
         return height;
     }
 
+    /**
+     * player height setter
+     * @param height height to height
+     */
     public void setHeight(int height) {
         this.height = height;
     }
