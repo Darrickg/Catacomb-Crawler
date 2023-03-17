1993f40b307dd7cf7250d06a1969121b5656c031
hanxic <hanxic@sfu.ca>
2023-03-17 02:56:49 -0700

    GameStateManager JavaDoc update

--- src/main/java/GameStates/GameStateManager.java
+++ src/main/java/GameStates/GameStateManager.java
@@ -3,27 +3,48 @@ package GameStates;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * game state manager with all fields as parameters create manager of game state
+ */
 public class GameStateManager extends JPanel {
     private GameState currentState;
 
+    /**
+     * state setter
+     * @param state set state
+     */
     public void setState(GameState state) {
         currentState = state;
         currentState.init();
 
     }
 
+    /**
+     * game state updater
+     */
     public void update() {
         currentState.update();
     }
 
+    /**
+     * game state render
+     */
     public void render() {
         currentState.render();
     }
 
+    /**
+     * game state getter
+     * @return current state
+     */
     public GameState getCurrentState() {
         return currentState;
     }
 
+    /**
+     * game state setter
+     * @param currentState state = current state
+     */
     public void setCurrentState(GameState currentState) {
         this.currentState = currentState;
     }
