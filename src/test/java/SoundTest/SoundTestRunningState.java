6da73e44838e8f2f214296f1c6f0c2b91f785329
dga48 <darrickgunawan@gmail.com>
2023-04-04 13:00:21 -0700

    added test for sounds in running state

---
+++ src/test/java/SoundTest/SoundTestRunningState.java
@@ -0,0 +1,129 @@
+package SoundTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.File;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+import org.junit.jupiter.api.Test;
+
+public class SoundTestRunningState {
+    
+    @Test
+    public void testRunningStateMusic() {
+        try {
+            AudioInputStream gameMusic = AudioSystem.getAudioInputStream(new File("assets/audio/gamemusic.wav"));
+            Clip gameMusicClip = AudioSystem.getClip();
+            gameMusicClip.open(gameMusic);
+
+            gameMusicClip.start();
+
+            Thread.sleep(gameMusicClip.getMicrosecondLength() / 1000);
+
+            gameMusicClip.stop();
+
+            assertTrue(true);
+        } catch (Exception e) {
+            fail("Error playing music: " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testDoorOpenSound() {
+        try {
+            AudioInputStream doorOpen = AudioSystem.getAudioInputStream(new File("assets/audio/dooropen.wav"));
+            Clip doorOpenClip = AudioSystem.getClip();
+            doorOpenClip.open(doorOpen);
+
+            doorOpenClip.start();
+
+            Thread.sleep(doorOpenClip.getMicrosecondLength() / 1000);
+
+            doorOpenClip.stop();
+
+            assertTrue(true);
+        } catch (Exception e) {
+            fail("Error playing music: " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testWinMusic() {
+        try {
+            AudioInputStream winMusic = AudioSystem.getAudioInputStream(new File("assets/audio/gamewin.wav"));
+            Clip winMusicClip = AudioSystem.getClip();
+            winMusicClip.open(winMusic);
+
+            winMusicClip.start();
+
+            Thread.sleep(winMusicClip.getMicrosecondLength() / 1000);
+
+            winMusicClip.stop();
+
+            assertTrue(true);
+        } catch (Exception e) {
+            fail("Error playing music: " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testGameOverSound() {
+        try {
+            AudioInputStream gameOver = AudioSystem.getAudioInputStream(new File("assets/audio/gameover.wav"));
+            Clip gameOverClip = AudioSystem.getClip();
+            gameOverClip.open(gameOver);
+
+            gameOverClip.start();
+
+            Thread.sleep(gameOverClip.getMicrosecondLength() / 1000);
+
+            gameOverClip.stop();
+
+            assertTrue(true);
+        } catch (Exception e) {
+            fail("Error playing music: " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testOpenChestSound() {
+        try {
+            AudioInputStream openChest = AudioSystem.getAudioInputStream(new File("assets/audio/openchest.wav"));
+            Clip openChestClip = AudioSystem.getClip();
+            openChestClip.open(openChest);
+
+            openChestClip.start();
+
+            Thread.sleep(openChestClip.getMicrosecondLength() / 1000);
+
+            openChestClip.stop();
+
+            assertTrue(true);
+        } catch (Exception e) {
+            fail("Error playing music: " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testCoinSound() {
+        try {
+            AudioInputStream coinSound = AudioSystem.getAudioInputStream(new File("assets/audio/coinsound.wav"));
+            Clip coinSoundClip = AudioSystem.getClip();
+            coinSoundClip.open(coinSound);
+
+            coinSoundClip.start();
+
+            Thread.sleep(coinSoundClip.getMicrosecondLength() / 1000);
+
+            coinSoundClip.stop();
+
+            assertTrue(true);
+        } catch (Exception e) {
+            fail("Error playing music: " + e.getMessage());
+        }
+    }
+}
