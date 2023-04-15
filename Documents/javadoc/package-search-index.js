04c431610b898b8925abb6e91b92e94355d33503
Mahyar <mahyaresharafi@gmail.com>
2023-04-14 18:27:44 -0700

    Added JavaDoc and JAR output to documents directory

---
+++ Documents/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"astar"},{"l":"Entity"},{"l":"GameStates"},{"l":"HealthBar"},{"l":"Item"},{"l":"ItemTest"},{"l":"Main"},{"l":"OperationTest"},{"l":"Rewards"},{"l":"SoundTest"},{"l":"tile"}];updateSearchResults();
